768eefc23f2ce142bce1c5b36ead70cae523dc5b
Haein <46235778+2haein@users.noreply.github.com>
2021-10-08 04:08:45 +0900

    Update multi-table.sh

--- multi-table.sh
+++ multi-table.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 i=1
 j=1
-while [ $i -le $1 ]
-do
-j=1
-	while [ $j -le $2 ]
+if [ $1 -ge 1 ] && [ $2 -ge 1 ]
+then
+	while [ $i -le $1 ]
 	do
-		result=`expr $i \* $j`
-		echo -n "$i*$j=$result "
-		
-		j=`expr $j + 1`
+	j=1
+		while [ $j -le $2 ]
+		do
+			result=`expr $i \* $j`
+			echo -n "$i*$j=$result "
+			j=`expr $j + 1`
+		done
+	echo " "
+	i=`expr $i + 1`
 	done
-echo " "
-i=`expr $i + 1`
-done
+else
+	echo "the input number is out of range"
+fi
+exit 0
